e9278ff60515538387d875d89ffacd9122225a73
Tobias <iso8859-1@gmx.de>
2012-07-22 17:58:11 +0200

    updated unit test

--- BufferHandlerTest/BufferHandlerTest.cpp
+++ BufferHandlerTest/BufferHandlerTest.cpp
@@ -8,11 +8,34 @@
 #include <boost/smart_ptr.hpp>
 #include "BufferHandler.h"
 
-BOOST_AUTO_TEST_CASE(alignedAccessTest)
+BOOST_AUTO_TEST_CASE(alignedAccessTestUILE8Bit)
 {
 	unsigned char buffer[10] = { 0,1,2,3,4,5,6,7,8,9};
 	auto h = CreateBufferHandler(8,8,UnsignedIntegerLittleEndian);
-	long value = h->ReadL(&buffer[0],sizeof(buffer));
-	BOOST_CHECK(value == 1);
+
+	{
+		auto value = h->ReadLL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 1);
+	}
+	{
+		auto value = h->ReadL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 1);
+	}
+	{
+		auto value = h->ReadULL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 1);
+	}
+	{
+		auto value = h->ReadUL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 1);
+	}
+	{
+		auto value = h->ReadF(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 1);
+	}
+	{
+		auto value = h->ReadD(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 1);
+	}
 }
 
