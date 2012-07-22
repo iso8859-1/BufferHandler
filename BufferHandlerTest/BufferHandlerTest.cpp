72423a7771c0179c79050f25ba0384ee31785e9c
Tobias <iso8859-1@gmx.de>
2012-07-22 18:12:10 +0200

    added lower boundary tests

--- BufferHandlerTest/BufferHandlerTest.cpp
+++ BufferHandlerTest/BufferHandlerTest.cpp
@@ -8,6 +8,86 @@
 #include <boost/smart_ptr.hpp>
 #include "BufferHandler.h"
 
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest8UILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,8,UnsignedIntegerLittleEndian);
+		auto value = h->ReadL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest8SILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,8,SignedIntegerLittleEndian);
+		auto value = h->ReadL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest16UILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,16,UnsignedIntegerLittleEndian);
+		auto value = h->ReadL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0x0100);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest16SILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,16,SignedIntegerLittleEndian);
+		auto value = h->ReadL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0x0100);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest32UILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,32,UnsignedIntegerLittleEndian);
+		auto value = h->ReadL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0x03020100);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest32SILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,32,SignedIntegerLittleEndian);
+		auto value = h->ReadL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0x03020100);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest64UILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,64,UnsignedIntegerLittleEndian);
+		auto value = h->ReadLL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0x0706050403020100);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(accessLowerBoundaryTest64SILE)
+{
+	unsigned char buffer[10] = {0,1,2,3,4,5,6,7,8,9};
+	{
+		auto h = CreateBufferHandler(0,64,SignedIntegerLittleEndian);
+		auto value = h->ReadLL(&buffer[0],sizeof(buffer));
+		BOOST_CHECK(value == 0x0706050403020100);
+	}
+}
+
 BOOST_AUTO_TEST_CASE(alignedAccessTestUILE8Bit)
 {
 	unsigned char buffer[10] = { 0,1,2,3,4,5,6,7,8,9};
